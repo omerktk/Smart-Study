c1884128597b75a47f67adbaec3fccc5e8f3ed5d
omerktk <greatomer876@gmail.com>
2022-08-12 15:50:25 +0500

    11

--- app/src/main/java/com/codelegacy/smartstudy/TeaLoginActivity.java
+++ app/src/main/java/com/codelegacy/smartstudy/TeaLoginActivity.java
@@ -2,29 +2,108 @@ package com.codelegacy.smartstudy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class TeaLoginActivity extends AppCompatActivity {
-    MaterialButton btn,btn2;
+
+    DatabaseReference ref;
+    EditText t_user,t_pass;
+    FirebaseAuth mAuth;
+    MaterialButton t_login,t_reg_red;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tea_login);
 
-        btn = (MaterialButton) findViewById(R.id.loginbtn76);
-        btn2 = (MaterialButton) findViewById(R.id.btn276);
+        mAuth = FirebaseAuth.getInstance();
 
-        btn.setOnClickListener(view -> {
-            startActivity(new Intent(getApplicationContext(),TeaDashActivity.class));
-            Toast.makeText(TeaLoginActivity.this, "Login Done", Toast.LENGTH_LONG).show();
+        t_user = findViewById(R.id.t_user);
+        t_pass = findViewById(R.id.t_pass);
+        t_login = (MaterialButton) findViewById(R.id.t_login);
+        t_reg_red = (MaterialButton) findViewById(R.id.t_reg_red);
+
+        //button 1 login
+        t_login.setOnClickListener(view -> {
+            userLogin();
         });
-        btn2.setOnClickListener(view -> {
-            startActivity(new Intent(getApplicationContext(),TeaRegActivity.class));
+
+
+        //button 2 change screen onclick
+        t_reg_red.setOnClickListener(view -> {
+            Intent reg = new Intent(TeaLoginActivity.this,TeaRegActivity.class);
+            startActivity(reg);
+        });
+
+    }
+
+    private void userLogin() {
+
+        String suser = t_user.getText().toString().trim();
+        String spass = t_pass.getText().toString().trim();
+
+        if(suser.isEmpty()){
+            t_user.setError("Make sure to fill this field");
+            t_user.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(suser).matches()){
+            t_user.setError("Email Is Not Vaild");
+            t_user.requestFocus();
+            return;
+        }
+
+
+        if(spass.isEmpty()){
+            t_pass.setError("Field Is Empty");
+            t_pass.requestFocus();
+            return;
+        }
+
+
+        if(spass.length() < 6){
+            t_pass.setError("Password is less then 6 words!");
+            t_pass.requestFocus();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(suser,spass).addOnSuccessListener(TeaLoginActivity.this, new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                startActivity(new Intent(getApplicationContext(),TeaDashActivity.class));
+                Toast.makeText(TeaLoginActivity.this, "Login Done", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        mAuth.signInWithEmailAndPassword(suser,spass).addOnFailureListener(TeaLoginActivity.this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(TeaLoginActivity.this, "Login Failed", Toast.LENGTH_LONG).show();
+            }
         });
     }
 }
--- app/src/main/java/com/codelegacy/smartstudy/TeaRegActivity.java
+++ app/src/main/java/com/codelegacy/smartstudy/TeaRegActivity.java
@@ -1,24 +1,93 @@
 package com.codelegacy.smartstudy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.material.button.MaterialButton;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class TeaRegActivity extends AppCompatActivity {
-
-    MaterialButton btn;
-
+        FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tea_reg);
-        btn = (MaterialButton) findViewById(R.id.regbtn);
+        mAuth = FirebaseAuth.getInstance();
+        EditText tname, tuser, tpass;
+        Button t_reg;
 
-        btn.setOnClickListener(view -> {
+        tname = findViewById(R.id.t_name);
+        tuser = findViewById(R.id.t_user);
+        tpass = findViewById(R.id.t_pass);
+        t_reg = findViewById(R.id.t_regbtn);
+
+        t_reg.setOnClickListener(view->{
+            String suser = tuser.getText().toString().trim();
+            String spass = tpass.getText().toString().trim();
+            String sname = tname.getText().toString().trim();
+
+            if(sname.isEmpty()){
+                tname.setError("Make sure to fill this field");
+                tname.requestFocus();
+                return;
+            }
+
+            if(suser.isEmpty()){
+                tuser.setError("Make sure to fill this field");
+                tuser.requestFocus();
+                return;
+            }
+
+            if(!Patterns.EMAIL_ADDRESS.matcher(suser).matches()){
+                tuser.setError("Email Is Not Vaild");
+                tuser.requestFocus();
+                return;
+            }
+
+
+            if(spass.isEmpty()){
+                tpass.setError("Field Is Empty");
+                tpass.requestFocus();
+                return;
+            }
+
+
+            if(spass.length() < 6){
+                tpass.setError("Password is less then 6 words!");
+                tpass.requestFocus();
+                return;
+            }
+
+
+
+            mAuth.createUserWithEmailAndPassword(tuser.getText().toString(),tpass.getText().toString()).addOnSuccessListener(TeaRegActivity.this, new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    Toast.makeText(TeaRegActivity.this, "inserted", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+
+            mAuth.createUserWithEmailAndPassword(tuser.getText().toString(),tpass.getText().toString()).addOnFailureListener(TeaRegActivity.this, new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(TeaRegActivity.this, "Reg Failed", Toast.LENGTH_LONG).show();
+                }
+            });
+            DatabaseReference t_db = FirebaseDatabase.getInstance("https://smart-study-cdbd4-default-rtdb.firebaseio.com/").getReference("teacher");
+            String t_id = System.currentTimeMillis()+"";
+            teacher_data t_data = new teacher_data(tname.getText().toString(),tuser.getText().toString(),tpass.getText().toString());
+            t_db.child(t_id).setValue(t_data);
             Toast.makeText(TeaRegActivity.this, "Reg Done", Toast.LENGTH_LONG).show();
             super.onBackPressed();
         });
