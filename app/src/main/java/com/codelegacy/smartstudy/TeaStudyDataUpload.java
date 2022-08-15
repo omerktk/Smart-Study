7c7774f6b06d14aa6dbda263a80ed81f4f3f3f64
omerktk <greatomer876@gmail.com>
2022-08-15 16:23:11 +0500

    1

--- app/src/main/java/com/codelegacy/smartstudy/TeaStudyDataUpload.java
+++ app/src/main/java/com/codelegacy/smartstudy/TeaStudyDataUpload.java
@@ -2,7 +2,6 @@ package com.codelegacy.smartstudy;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.util.Patterns;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -16,8 +15,6 @@ import com.google.firebase.database.FirebaseDatabase;
 public class TeaStudyDataUpload extends AppCompatActivity {
     MaterialCardView btn;
     ProgressDialog pd;
-    EditText name;
-    EditText mail;
     DatabaseReference ref = FirebaseDatabase.getInstance("https://smart-study-cdbd4-default-rtdb.firebaseio.com/").getReference("StudyData");
 
 
@@ -41,16 +38,15 @@ public class TeaStudyDataUpload extends AppCompatActivity {
     private void uploadImage() {
         String maxid = System.currentTimeMillis()+"" ;
 
+        pd = new ProgressDialog(this);
+        pd.setTitle("Uploading File...");
+        pd.show();
+        EditText name = findViewById(R.id.username1);
+        EditText  mail = findViewById(R.id.password1);
 
-            pd = new ProgressDialog(this);
-            pd.setTitle("Uploading File...");
-            pd.show();
-            name = findViewById(R.id.username1);
-            mail = findViewById(R.id.password1);
-
-            //data
-            JavaModal member = new JavaModal(name.getText().toString(),mail.getText().toString(), "null");
-            ref.child(maxid).setValue(member);
+        //data
+        JavaModal member = new JavaModal(name.getText().toString(),mail.getText().toString(), "null");
+        ref.child(maxid).setValue(member);
         pd.hide();
         name.setText("");
         mail.setText("");
